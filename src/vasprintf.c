3d699484c4e06b99076a63b3b3bd2d89c0397152
Skerlet Development Team <skerlet@swc.toshiba.co.jp>
2015-04-13 21:58:31 +0900

    git-debimport file_5.22+15.orig.tar.xz

--- src/vasprintf.c
+++ src/vasprintf.c
@@ -633,11 +633,15 @@ int vasprintf(char **ptr, const char *format_string, va_list vargs)
 #ifdef va_copy
   va_copy (s.vargs, vargs);
 #else
-#ifdef __va_copy
+# ifdef __va_copy
   __va_copy (s.vargs, vargs);
-#else
+# else
+#  ifdef WIN32
+  s.vargs = vargs;
+#  else
   memcpy (&s.vargs, &vargs, sizeof (s.va_args));
-#endif /* __va_copy */
+#  endif /* WIN32 */
+# endif /* __va_copy */
 #endif /* va_copy */
   s.maxlen = (size_t)INT_MAX;
 
